699e6af7280f1a2c0e5e6461da435c1993bdf7fa
jhess <juhess88@gmail.com>
2017-06-18 20:32:06 -0400

    added persisted state

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,8 +1,19 @@
 import { createStore } from 'redux';
 import todoApp from './reducers/index';
 
+const persistedState = {
+  todos: [{
+    id: '0',
+    text: 'Welcome back',
+    completed: false,
+  }],
+};
+
 const configureStore = () => {
-  const store = createStore(todoApp);
+  const store = createStore(
+    todoApp,
+    persistedState
+  );
 
   return store;
 }
