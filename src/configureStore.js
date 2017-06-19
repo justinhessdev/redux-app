d3309ea08eb2cb262441126ce5eba72244a5df81
jhess <juhess88@gmail.com>
2017-06-19 10:33:08 -0400

    added removeItem method in config store to remove saved state when needed, currently commented out

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,6 +4,11 @@ import todoApp from './reducers/index';
 import { loadState, saveState } from './localStorage';
 
 const configureStore = () => {
+  /*
+    removeItem('state') removes the savedState from localStorage
+    Just use for testing if toooo many things are saved and it gets annoying...
+  */
+  // localStorage.removeItem('state');
   const persistedState = loadState();
 
   const store = createStore(
